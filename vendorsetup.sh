48caa80e2e1703cd80c102c86d19e0cbe7fd05c6
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-12-19 12:26:26 +0530

    beryllium: Stick with Clang 17

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -30,3 +30,6 @@ git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a p
 rm -rf vendor/bcr
 git clone https://github.com/Chaitanyakm/vendor_bcr.git -b main vendor/bcr --depth=1
 
+# Clang
+rm -rf prebuilts/clang/host/linux-x86/clang-r487747c
+git clone https://gitlab.com/kei-space/clang/r487747c.git -b master prebuilts/clang/host/linux-x86/clang-r487747c --depth=1
