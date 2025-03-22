3ecbb65b5623961eb9454c122e1116ed5b0d1611
kyasu <kyasu.dev@gmail.com>
2025-03-22 10:21:32 +0900

    beryllium: Disable UFFD GC
    Change-Id: Icc8b5c008558b824a93b46f4ccb3311bbce653a7
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -85,6 +85,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := false
+
 # Matlog
 ifeq ($(TARGET_BUILD_VARIANT),user)
 TARGET_DISABLE_MATLOG := true
